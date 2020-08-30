b1b3653f25b07828427f5d3b7f091d0610ed7cde
Kim Hallberg <hallberg.kim@gmail.com>
2020-08-30 23:52:51 +0200

    Update main.py
    Let's see copy-pasting from the issue solves this ...

--- .github/actions/cat-facts/src/main.py
+++ .github/actions/cat-facts/src/main.py
@@ -9,11 +9,11 @@ r_obj_list = r.json()["all"]
 fact_list = []
 
 for fact in r_obj_list:
-  fact_list.append(fact["text"])
+    fact_list.append(fact["text"])
 
 
 def select_random_fact(fact_arr):
-  return fact_arr[random.randint(0, len(fact_list)+1)]
+    return fact_arr[random.randint(0, len(fact_list)+1)]
 
 random_fact = select_random_fact(fact_list)
 
